5429b0903b4185d6efb48ca9b14b9453164d04c9
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-03-30 18:28:16 +0530

    app.js done'

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "https://dreaminalgo.pages.dev",
   "https://dreaminalgo-frontend.vercel.app/",
-  "https://algo.dreamintraders.in/"
+  "https://algo.dreamintraders.in"
 ];
 
 app.use(
