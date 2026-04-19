d961dcea731e1e8159053280befc86a7f66b8fd7
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-04-19 15:31:29 +0530

    asfv

--- src/modules/realtradegroups/realTradeGroups.controller.js
+++ src/modules/realtradegroups/realTradeGroups.controller.js
@@ -103,38 +103,45 @@ export const getLatestTrade = async (req, res) => {
   }
 };
 
-
-
 export const getOpenTrades = async (req, res) => {
   try {
     const { user_id, strategy_id, broker_id, date } = req.body;
 
     const result = await pool.query(
-      `
-      SELECT *
-      FROM real_trade_groups t1
-      WHERE t1.user_id = $1
-        AND t1.strategy_id = $2
-        AND t1.broker_id = $3
-        AND t1.trade_date = $4
-        AND t1.event_type = 'ENTRY'
-        AND NOT EXISTS (
-          SELECT 1
-          FROM real_trade_groups t2
-          WHERE t2.user_id = t1.user_id
-            AND t2.strategy_id = t1.strategy_id
-            AND t2.broker_id = t1.broker_id
-            AND t2.trade_date = t1.trade_date
-            AND t2.symbol = t1.symbol
-            AND t2.leg_name = t1.leg_name
-            AND t2.event_type = 'EXIT'
-            AND t2.timestamp > t1.timestamp
-        )
-      ORDER BY t1.timestamp DESC
-      `,
-      [user_id, strategy_id, broker_id, date]
-    );
+  `
+  SELECT 
+    t1.*,
+    b.broker_name,
+    b.credentials
 
+  FROM real_trade_groups t1
+
+  LEFT JOIN broker_accounts b
+    ON t1.broker_id::uuid = b.id
+
+  WHERE t1.user_id = $1
+    AND t1.strategy_id = $2
+    AND t1.broker_id = $3
+    AND t1.trade_date = $4
+    AND t1.event_type = 'ENTRY'
+
+    AND NOT EXISTS (
+      SELECT 1
+      FROM real_trade_groups t2
+      WHERE t2.user_id = t1.user_id
+        AND t2.strategy_id = t1.strategy_id
+        AND t2.broker_id = t1.broker_id
+        AND t2.trade_date = t1.trade_date
+        AND t2.symbol = t1.symbol
+        AND t2.leg_name = t1.leg_name
+        AND t2.event_type = 'EXIT'
+        AND t2.timestamp > t1.timestamp
+    )
+
+  ORDER BY t1.timestamp DESC
+  `,
+  [user_id, strategy_id, broker_id, date]
+);
     res.json(result.rows);
 
   } catch (err) {
